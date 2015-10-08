48c171c898523a4dc81843ae45462b0f7503c6e3
hibbes <marek@czernohous.de>
2015-10-08 09:09:56 +0200

    TestGeomewtricObjects implementiert

--- src/GeometricObject.java
+++ src/GeometricObject.java
@@ -27,6 +27,8 @@ public class GeometricObject {
 	
 	public GeometricObject(double width){this(width,width);}
 	
+	public GeometricObject(){this(10);}
+	
 	public GeometricObject(Vertex pos){this(0,0,pos);}
 	
     public double getWidth(){return width;}
