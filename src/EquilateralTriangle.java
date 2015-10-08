1b404bd1f8621219f38566b64393cfb242228d95
hibbes <marek@czernohous.de>
2015-10-08 10:30:11 +0200

    Dreiecksklasse hinzugefügt

---
+++ src/EquilateralTriangle.java
@@ -0,0 +1,21 @@
+
+public class EquilateralTriangle extends GeometricObject{
+
+	public EquilateralTriangle(double w, double h, Vertex pos){
+		super(w,h,pos);
+	}
+	
+	public EquilateralTriangle(double w, double h, double x, double y){
+		super(w,h,new Vertex(x,y));
+	}
+	@Override public double area(){return (width/height)/2;}
+
+	public @Override String toString(){
+		return "EquilateralTriangle("+super.toString()+")";
+	}
+	
+	public @Override boolean equals(Object that){
+		return (that instanceof EquilateralTriangle) && super.equals(that);
+	}
+}
+
