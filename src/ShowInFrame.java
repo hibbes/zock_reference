b209ca8f78f12c5189cff72a1fb6794ae35e442a
hibbes <marek@czernohous.de>
2015-10-08 10:56:10 +0200

    Frames

---
+++ src/ShowInFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class ShowInFrame {
+
+	public static void show(String title,JComponent c) {
+		JFrame f=new JFrame(title);
+		f.add(c);
+		f.pack();
+		f.setVisible(true);
+	}
+		public static void show(JComponent c){show("",c);}
+
+}
+
+
