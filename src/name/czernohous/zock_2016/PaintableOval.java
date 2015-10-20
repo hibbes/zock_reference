93bf9c60b1b69ceca416464ac7c551703b6e5aa8
hibbes <marek@czernohous.de>
2015-10-20 11:29:37 +0200

    PaintablesPanel (Listen

--- src/name/czernohous/zock_2016/PaintableOval.java
+++ src/name/czernohous/zock_2016/PaintableOval.java
@@ -6,6 +6,11 @@ public class PaintableOval extends SimpleOval implements Paintable {
 		super(width,height,x,y,color);
 	}
 	
+	public PaintableOval(double width, double height, double x, double y){
+		this(width,height,x,y,Color.black);
+	}
+	
+	
 	public void paintTo(java.awt.Graphics g){
 		g.setColor(color);
 		g.fillOval((int)pos.x, (int)pos.y, (int)width, (int)height);
