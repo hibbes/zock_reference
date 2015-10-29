f21986c2b5508fedf8058481f878cf4afa0f1600
Marek Czernohous <marek@czernohous.de>
2015-10-29 11:32:46 +0100

    Bugfixes

--- src/name/czernohous/zock_2016/Movable.java
+++
@@ -1,6 +0,0 @@
-package name.czernohous.zock_2016;
-
-interface Movable {
-	void move();
-	void turn();
-}
--- src/name/czernohous/zock_2016/Paintable.java
+++
@@ -1,7 +0,0 @@
-package name.czernohous.zock_2016;
-
-public interface Paintable {
-
-	void paintTo(java.awt.Graphics g);
-	
-}
