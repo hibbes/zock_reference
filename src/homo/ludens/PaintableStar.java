9940289750f68e1bc2362e20c685b075297e6b15
hibbes <marek@czernohous.de>
2026-03-31 23:08:21 +0200

    refactor: rename package czernohous→homo.ludens (PaintableStar.java)

---
+++ src/homo/ludens/PaintableStar.java
@@ -0,0 +1,16 @@
+package homo.ludens;
+import java.awt.Color;
+
+public class PaintableStar extends SimpleStar implements Paintable{
+	
+		public PaintableStar(Vertex pos, Color color){
+			super(pos, color);
+		}
+		
+		public void paintTo(java.awt.Graphics g){
+			g.setColor(color);
+			g.drawPolygon(xPoints, yPoints, nPoints);
+		}
+	}
+
+
