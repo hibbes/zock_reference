23f54d5fa753aa682084037b5faa09a7298980c4
hibbes <marek@czernohous.de>
2026-03-31 23:08:24 +0200

    refactor: rename package czernohous→homo.ludens (PaintablesPanel.java)

---
+++ src/homo/ludens/PaintablesPanel.java
@@ -0,0 +1,21 @@
+package homo.ludens;
+import homo.ludens.util.ShowInFrame;
+import java.util.*;
+
+public class PaintablesPanel extends SizedPanel{
+	List<Paintable> pas;
+	
+public PaintablesPanel(List<Paintable> pas){this.pas=pas;}	
+	
+public void paintComponent(java.awt.Graphics g){
+	for (Paintable pa:pas) pa.paintTo(g);
+}
+	public static void main(String[] args) {
+		List<Paintable> ps = new ArrayList<Paintable>();
+		ps.add(new PaintableOval(100,50,30,50));
+		ps.add(new PaintableOval(100,50,130,150));
+		
+		ShowInFrame.show("Ovale",new PaintablesPanel(ps));
+	}
+
+}
