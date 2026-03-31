01484adb2ef3f91a2c15ec7bc9f81e993058f137
hibbes <marek@czernohous.de>
2026-03-31 23:08:29 +0200

    refactor: rename package czernohous→homo.ludens (SimplePaintableRectangle.java)

---
+++ src/homo/ludens/SimplePaintableRectangle.java
@@ -0,0 +1,16 @@
+package homo.ludens;
+import java.awt.Color;
+import java.awt.Graphics;
+public class SimplePaintableRectangle extends SimpleRectangle implements Paintable{
+
+			public SimplePaintableRectangle(double width, double height, double x, double y, Color color){
+			super(width,height,new Vertex(x,y),color);
+		}
+		
+		public void paintTo(java.awt.Graphics g){ 
+			g.setColor(color);
+			g.fillRect((int)pos.x, (int)pos.y, (int)width, (int)height);
+		}
+	}
+	
+
