9ed8c0d07759bc14887b79965165e21ed88e7dfa
hibbes <marek@czernohous.de>
2026-03-31 23:07:55 +0200

    refactor: rename package czernohous→homo.ludens (JFrameTest.java)

---
+++ src/homo/ludens/JFrameTest.java
@@ -0,0 +1,10 @@
+package homo.ludens;
+import javax.swing.JFrame;
+
+public class JFrameTest {
+   public static void main(String []args){
+	JFrame f=new JFrame("Hello World!");
+	f.setVisible(true);
+   }
+   
+}
