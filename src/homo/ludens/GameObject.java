e2b42622c1b0d38b40fe3f40f2df6888b1ac527c
hibbes <marek@czernohous.de>
2026-03-31 23:07:48 +0200

    refactor: rename package czernohous→homo.ludens (GameObject.java)

---
+++ src/homo/ludens/GameObject.java
@@ -0,0 +1,14 @@
+package homo.ludens;
+
+public interface GameObject extends Movable, Paintable{
+	double getWidth();
+	double getHeight();
+	Vertex getPos();
+	Vertex getDeltaPos();
+	void setDeltaPos(Vertex v);
+	boolean isLargerThan(GameObject that);
+	boolean isLeftOf(GameObject that);
+	boolean isAbove(GameObject that);
+	boolean touches(GameObject that);
+
+}
